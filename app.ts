7a1958238d13470e267bc7a502e95141fdeff3bb
betsabe2009 <zoe24474@cbtis75.edu.mx>
2026-09-23 08:18:43 -0600

    Enhance App component with pagination and details
    Added pagination and movie detail features to the App component.

--- app.ts
+++ app.ts
@@ -16,17 +16,29 @@ export class App {
   searchQuery: string = '';
   movies: any[] = [];
   errorMessage: string = '';
+ 
+  currentPage: number = 1;
+  totalResults: number = 0;
+  get totalPages(): number {
+    return Math.ceil(this.totalResults / 10); // OMDb devuelve 10 resultados por página
+  }
 
-  onSearch() {
+  selectedMovie: any = null;
+
+  onSearch(page: number = 1) {
     if (!this.searchQuery.trim()) return;
 
-    this.omdbService.searchMovies(this.searchQuery).subscribe({
+    this.currentPage = page;
+
+    this.omdbService.searchMovies(this.searchQuery, page).subscribe({
       next: (data) => {
         if (data.Response === 'True') {
           this.movies = data.Search;
+          this.totalResults = Number(data.totalResults);
           this.errorMessage = '';
         } else {
           this.movies = [];
+          this.totalResults = 0;
           this.errorMessage = data.Error;
         }
       },
@@ -35,5 +47,25 @@ export class App {
         console.error(err);
       }
     });
+  }
+
+  goToPage(page: number) {
+    if (page < 1 || page > this.totalPages) return;
+    this.onSearch(page);
+  }
+
+  verDetalles(imdbID: string) {
+    this.omdbService.getMovieDetails(imdbID).subscribe({
+      next: (data) => {
+        if (data.Response === 'True') {
+          this.selectedMovie = data;
+        }
+      },
+      error: (err) => console.error(err)
+    });
+  }
+
+  cerrarDetalles() {
+    this.selectedMovie = null;
   }
 }
