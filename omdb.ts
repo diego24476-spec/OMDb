8ede9ee3ddbe7f435d057926403f225248527cd1
betsabe2009 <zoe24474@cbtis75.edu.mx>
2026-09-23 08:19:02 -0600

    Update omdb.ts

--- omdb.ts
+++ omdb.ts
@@ -10,11 +10,21 @@ export class OmdbService {
   private apiUrl = 'https://www.omdbapi.com/';
   private apiKey = 'ff282712'; 
 
-  searchMovies(title: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}?s=${encodeURIComponent(title)}&apikey=${this.apiKey}`);
+ searchMovies(title: string, page: number = 1): Observable<any> {
+    return this.http.get(
+      `${this.apiUrl}?s=${encodeURIComponent(title)}&page=${page}&apikey=${this.apiKey}`
+    );
+  }
+
+  getMovieByTitle(title: string): Observable<any> {
+    return this.http.get(
+      `${this.apiUrl}?t=${encodeURIComponent(title)}&plot=full&apikey=${this.apiKey}`
+    );
   }
 
   getMovieDetails(imdbID: string): Observable<any> {
-    return this.http.get(`${this.apiUrl}?i=${imdbID}&plot=full&apikey=${this.apiKey}`);
+    return this.http.get(
+      `${this.apiUrl}?i=${imdbID}&plot=full&apikey=${this.apiKey}`
+    );
   }
 }
